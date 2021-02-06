ded8529fa658af6c6f5d52a8f370c5ec2d9ed349
chaerulzzz <chaerul.a12@gmail.com>
2021-02-07 00:58:44 +0700

    - pembuatan general page - penambahan assets

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:foodmarket/shared/shared.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:supercharged/supercharged.dart';
 
 part 'general_page.dart';
