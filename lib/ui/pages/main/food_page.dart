c124119378d710f3fab6c0afb992b200ee9141b1
chaerulzzz <chaerul.a12@gmail.com>
2021-02-08 15:55:51 +0700

    - penambahan custom tab bar widget

--- lib/ui/pages/main/food_page.dart
+++ lib/ui/pages/main/food_page.dart
@@ -6,6 +6,8 @@ class FoodPage extends StatefulWidget {
 }
 
 class _FoodPageState extends State<FoodPage> {
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return ListView(
@@ -17,7 +19,7 @@ class _FoodPageState extends State<FoodPage> {
               color: Colors.white,
               height: 100,
               width: double.infinity,
-              child:Row(
+              child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Column(
@@ -25,19 +27,20 @@ class _FoodPageState extends State<FoodPage> {
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text("FoodMarket", style: blackFontStyle),
-                      Text("Lets get some food", style: greyFontStyle.copyWith(fontWeight: FontWeight.w300))
+                      Text("Lets get some food",
+                          style: greyFontStyle.copyWith(
+                              fontWeight: FontWeight.w300))
                     ],
                   ),
                   Container(
                     width: 50,
                     height: 50,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(8),
-                      image: DecorationImage(
-                        image: NetworkImage("https://i.pinimg.com/474x/8a/f4/7e/8af47e18b14b741f6be2ae499d23fcbe.jpg"),
-                        fit: BoxFit.cover
-                      )
-                    ),
+                        borderRadius: BorderRadius.circular(8),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                                "https://i.pinimg.com/474x/8a/f4/7e/8af47e18b14b741f6be2ae499d23fcbe.jpg"),
+                            fit: BoxFit.cover)),
                   ),
                 ],
               ),
@@ -49,18 +52,47 @@ class _FoodPageState extends State<FoodPage> {
                 scrollDirection: Axis.horizontal,
                 children: [
                   Row(
-                    children: mockFoods.map((e) => Padding(
-                      padding: EdgeInsets.only(
-                        left: (e == mockFoods[0]) ? defaultMargin : 0,
-                        right: defaultMargin
-                      ),
-                      child: FoodCard(e),
-                    )).toList(),
+                    children: mockFoods
+                        .map((e) => Padding(
+                              padding: EdgeInsets.only(
+                                  left: (e == mockFoods[0]) ? defaultMargin : 0,
+                                  right: defaultMargin),
+                              child: FoodCard(e),
+                            ))
+                        .toList(),
                   )
                 ],
               ),
             )
           ],
+        ),
+        Container(
+          width: double.infinity,
+          color: Colors.white,
+          child: Column(
+            children: [
+              CustomTabBar(
+                  titles: ["New", "Popular", "Recommended"],
+                  selectedIndex: selectedIndex,
+                  onTap: (index) {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  }),
+              SizedBox(height: 16),
+              Builder(builder: (_) {
+                String body = (selectedIndex == 0)
+                    ? "New Taste Body"
+                    : (selectedIndex == 1)
+                        ? "Popular Body"
+                        : "Recommend Body";
+
+                return Center(
+                  child: Text(body, style: blackFontStyle2),
+                );
+              })
+            ],
+          ),
         )
       ],
     );
