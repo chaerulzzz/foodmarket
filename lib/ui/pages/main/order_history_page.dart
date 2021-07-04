1b6d62fc5cbcbaec4cb687b2f06d3775653ad43f
chaerulzzz <chaerul.a12@gmail.com>
2021-07-04 19:00:26 +0700

    update order history page, penyesuaian dengan user yg login

--- lib/ui/pages/main/order_history_page.dart
+++ lib/ui/pages/main/order_history_page.dart
@@ -7,74 +7,82 @@ class OrderHistoryPage extends StatefulWidget {
 
 class _OrderHistoryPageState extends State<OrderHistoryPage> {
   int selectedIndex = 0;
-  List<Transaction> inProgress = mockTransactions.where((element) =>
-      element.status == TransactionStatus.on_delivery ||
-      element.status == TransactionStatus.pending).toList();
-  List<Transaction> inPast = mockTransactions.where((element) =>
-      element.status == TransactionStatus.cancelled ||
-      element.status == TransactionStatus.delivered).toList();
 
   @override
   Widget build(BuildContext context) {
-    if (inProgress.length == 0 && inPast.length == 0) {
-      return IllustrationPage(
-        title: 'Ouch! Hungry',
-        subtitle: 'Seems you like have not\n ordered any food yet',
-        picturePath: 'assets/love_burger.png',
-        buttonTap1: () {},
-        buttonTitle1: 'Find Foods',
-      );
-    } else{
-      double listItemWidth = MediaQuery.of(context).size.width - 2 * defaultMargin;
-      return ListView(
-        children: [
-          Column(
-            children: [
-              Container(
-                height: 100,
-                width: double.infinity,
-                margin: EdgeInsets.only(bottom: defaultMargin),
-                padding: EdgeInsets.symmetric(horizontal: defaultMargin),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
+    return BlocBuilder<TransactionCubit, TransactionState>(
+      builder: (_, state) {
+        if (state is TransactionLoaded) {
+          if (state.transaction.length == 0) {
+            return IllustrationPage(
+              title: 'Ouch! Hungry',
+              subtitle: 'Seems you like have not\n ordered any food yet',
+              picturePath: 'assets/love_burger.png',
+              buttonTap1: () {},
+              buttonTitle1: 'Find Foods',
+            );
+          } else{
+            double listItemWidth = MediaQuery.of(context).size.width - 2 * defaultMargin;
+            return ListView(
+              children: [
+                Column(
                   children: [
-                    Text("Your Orders", style: blackFontStyle),
-                    Text("Wait for the best meals", style: greyFontStyle.copyWith(fontWeight: FontWeight.w300))
-                  ],
-                ),
-              ),
-              Container(
-                width: double.infinity,
-                color: Colors.white,
-                child: Column(
-                  children: [
-                    CustomTabBar(titles: ["In Progress", "Past Orders"],
-                      selectedIndex: selectedIndex,
-                      onTap: (index) {
-                        setState(() {
-                          selectedIndex = index;
-                        });
-                      },
+                    Container(
+                      height: 100,
+                      width: double.infinity,
+                      margin: EdgeInsets.only(bottom: defaultMargin),
+                      padding: EdgeInsets.symmetric(horizontal: defaultMargin),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Your Orders", style: blackFontStyle),
+                          Text("Wait for the best meals", style: greyFontStyle.copyWith(fontWeight: FontWeight.w300))
+                        ],
+                      ),
                     ),
-                    SizedBox(
-                      height: 16
-                    ),
-                    Column(
-                      children: (selectedIndex == 0 ? inProgress : inPast)
-                          .map((e) => Padding(
-                            padding: const EdgeInsets.only(bottom: 16, left: defaultMargin, right: defaultMargin),
-                            child: OrderListItem(transaction: e, itemWidth: listItemWidth)))
-                          .toList(),
+                    Container(
+                      width: double.infinity,
+                      color: Colors.white,
+                      child: Column(
+                        children: [
+                          CustomTabBar(titles: ["In Progress", "Past Orders"],
+                            selectedIndex: selectedIndex,
+                            onTap: (index) {
+                              setState(() {
+                                selectedIndex = index;
+                              });
+                            },
+                          ),
+                          SizedBox(
+                            height: 16
+                          ),
+                          Builder(builder: (_) {
+                            List<Transaction> transactions = (selectedIndex == 0) ? state.transaction.where((element) =>
+                              element.status == TransactionStatus.on_delivery ||
+                              element.status == TransactionStatus.pending).toList() : state.transaction.where((element) =>
+                              element.status == TransactionStatus.cancelled ||
+                              element.status == TransactionStatus.delivered).toList();
+                            return Column(
+                              children: transactions
+                                  .map((e) => Padding(
+                                    padding: const EdgeInsets.only(bottom: 16, left: defaultMargin, right: defaultMargin),
+                                    child: OrderListItem(transaction: e, itemWidth: listItemWidth)))
+                                  .toList(),
+                            );
+                          })
+                        ],
+                      ),
                     )
                   ],
-                ),
-              )
-            ],
-          )
-        ],
-      );
-    }
-    
+                )
+              ],
+            );
+          }
+        } else {
+          return Center(child: loadingIndicator);
+        }
+      },
+    );
   }
 }
